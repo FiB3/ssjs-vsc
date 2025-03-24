8c86f2a6704d9ed960c3de4aa1061ce359602b07
Filip Bostik <filip.bostik@gmail.com>
2025-03-24 19:31:58 +0100

    Update packages, change to Ingestion Connection String for telemetry

--- src/telemetry.js
+++ src/telemetry.js
@@ -1,13 +1,19 @@
 const vscode = require('vscode');
 const TelemetryReporter = require('@vscode/extension-telemetry').default;
 const logger = require('./auxi/logger');
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '..', '.env') });
 
-const KEY = '8c0a3736-8ee0-423d-9c4f-e207ae935339';
+// Connection string for Application Insights - only used in development
+const CONNECTION_STRING = process.env.SSJS_VSC_TELEMETRY;
 const TELEMETRY_DEV_OFF = true; // disables telemetry in dev mode - logs via logger instead
 
 class TelemetryHandler {
 	constructor() {
-		this.reporter = new TelemetryReporter(KEY);
+		this.reporter = CONNECTION_STRING ? new TelemetryReporter(CONNECTION_STRING) : null;
+		if (!this.reporter) {
+			logger.warn('No telemetry reporter found. Please check your .env file.');
+		}
 	}
 
 	/**
@@ -15,9 +21,12 @@ class TelemetryHandler {
 	 * @param {Object} context
 	 */
 	init(context) {
-		context.subscriptions.push(this.reporter);
+		if (this.reporter) {
+			context.subscriptions.push(this.reporter);
+		}
 		this.isProd = context.extensionMode === vscode.ExtensionMode.Production;
 		logger.debug(`Telemetry logging in Prod mode: ${this.isProd}, logging OFF: ${TELEMETRY_DEV_OFF}`);
+		this.reporter.sendTelemetryEvent('test', { isTest: true});
 	}
 
 	/**
@@ -32,7 +41,9 @@ class TelemetryHandler {
 			logger.info(`Telemetry.log: ${eventName}`, properties, measurements);
 			return;
 		}
-		this.reporter.sendTelemetryEvent(eventName, properties, measurements);
+		if (this.reporter) {
+			this.reporter.sendTelemetryEvent(eventName, properties, measurements);
+		}
 	}
 
 	/**
@@ -47,14 +58,18 @@ class TelemetryHandler {
 			logger.error(`Telemetry.error: ${errorName}`, properties, measurements);
 			return;
 		}
-		this.reporter.sendTelemetryErrorEvent(errorName, properties, measurements);
+		if (this.reporter) {
+			this.reporter.sendTelemetryErrorEvent(errorName, properties, measurements);
+		}
 	}
 
 	/**
 	 * Dispose of the reporter.
 	 */
 	dispose() {
-		this.reporter.dispose();
+		if (this.reporter) {
+			this.reporter.dispose();
+		}
 	}
 }
 
